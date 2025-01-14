cc5886a8b579bd1751c7820b968058d23ae99ad9
NiveshPole18 <89723616+NiveshPole18@users.noreply.github.com>
2025-01-14 21:33:12 +0530

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,11 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors({
+  origin: 'https://frontendreddit-cin1.vercel.app/', // Frontend URL
+  methods: 'GET,POST,PUT,DELETE',
+  credentials: true,
+}));
 app.use(express.json());
 
 app.use('/api/posts', postsRouter);
